9d6db6f7d261e0e11667967dedd796755f4f0fca
João Lucas Mazetti <jlmazetti03@hotmail.com>
2025-06-14 01:15:24 -0300

    feat & fix - Inserindo arquivos security, realizando testes, builds e mudanças conforme funçoes do jwt

--- src/main/java/com/example/trabalhojwt/repository/RoleRepository.java
+++ src/main/java/com/example/trabalhojwt/repository/RoleRepository.java
@@ -1,10 +1,11 @@
 package com.example.trabalhojwt.repository;
 
+import com.example.trabalhojwt.model.ERole;
 import com.example.trabalhojwt.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);
+    Optional<Role> findByRole(ERole role);
 }
