bd892a90c2125179cc5785edc5450a885fa66b24
João Lucas Mazetti <jlmazetti03@hotmail.com>
2025-06-13 23:22:21 -0300

    feat: Iniciando spring boot e inserindo dependencia JWT

---
+++ src/main/java/com/example/trabalhojwt/TrabalhojwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.trabalhojwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhojwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhojwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trabalhojwt/TrabalhojwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabalhojwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhojwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
