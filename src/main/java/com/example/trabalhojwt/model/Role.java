f66522fccd4bc5c302aedbe1ad537e887d7f22cb
João Lucas Mazetti <jlmazetti03@hotmail.com>
2025-06-13 23:34:55 -0300

    feat - Criando as entidades Role

---
+++ src/main/java/com/example/trabalhojwt/model/Role.java
@@ -0,0 +1,38 @@
+package com.example.trabalhojwt.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ERole role;
+
+    public Role() {
+    }
+
+    public Role(ERole role) {
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ERole getRole() {
+        return role;
+    }
+
+    public void setRole(ERole role) {
+        this.role = role;
+    }
+}
